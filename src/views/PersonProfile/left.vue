fa9464a52be59a508182ca5fd3ed6e54924489df
fish <fish.yuzhupeng@goertek.com>
2025-12-29 20:52:21 +0800

    update

--- src/views/PersonProfile/left.vue
+++ src/views/PersonProfile/left.vue
@@ -48,7 +48,7 @@
           <p style="margin-left: 16px">{{ $t('workExperience.companies.lingyi.name') }}</p>
         </div>
       <div>
-                <span style="color: gray">{{ $t('workExperience.companies.goertek.period') }}</span>
+          <span style="color: gray">{{ $t('workExperience.companies.goertek.period') }}</span>
           <p style="margin-left: 16px">{{ $t('workExperience.companies.goertek.name') }}</p>
       </div>
 
